7f9d5e62c76cd9d2285e34cf251b41d77ea0ce5b
bakedpotatolord <jeffy9745@gmail.com>
2023-09-14 19:16:01 -0600

    prettify account creation

--- app/pages/CreateAcct.vue
+++ app/pages/CreateAcct.vue
@@ -1,61 +1,8 @@
-<template>
-	<h1>Account Creation</h1>
-	<form type="POST" v-on:submit="e => submitForm(e)">
-		<label for="name">
-			Name (preferably your real one)
-		</label>
-		<br />
-		<input
-			type="text"
-			name="name"
-			v-model="name"
-			required
-		/>
-		<br />
-		<label for="title">Title (your job on the team)</label>
-		<br />
-		<input
-			type="text"
-			name="title"
-			v-model="title"
-			required
-		/>
-		<br />
-		<label for="pin">pin</label>
-		<br />
-		<input
-			type="number"
-			name="pin"
-			min="0"
-			max="9999"
-			v-model="pin"
-			required
-		/>
-		<br />
-		<label for="pin2">retype pin</label>
-		<br />
-		<input
-			type="number"
-			name="pin2"
-			min="0"
-			max="9999"
-			v-model="pin2"
-			required
-		/>
-		<br />
-		<input type="submit" value="submit" />
-	</form>
-	<p>
-		Already have an account?
-		<router-link to="/login">login</router-link>
-	</p>
-</template>
-
 <script setup lang="ts">
 const name = ref('')
 const title = ref('')
-const pin = ref(0)
-const pin2 = ref(0)
+const pin = ref(null)
+const pin2 = ref(null)
 
 async function submitForm(e: Event) {
 	e.preventDefault()
@@ -79,3 +26,79 @@ async function submitForm(e: Event) {
 	})
 }
 </script>
+
+<template>
+	<h1>Account Creation</h1>
+	<form type="POST" v-on:submit="e => submitForm(e)">
+		<label for="name">
+			Name (preferably your real one)
+		</label>
+		<br />
+		<input
+			type="text"
+			name="name"
+			v-model="name"
+			required
+		/>
+		<br />
+		<label for="title">Title (your job on the team)</label>
+		<br />
+		<input
+			type="text"
+			name="title"
+			v-model="title"
+			required
+		/>
+		<br />
+		<label for="pin">Pin</label>
+		<br />
+		<input
+			type="number"
+			name="pin"
+			min="0"
+			max="9999"
+			v-model="pin"
+			required
+		/>
+		<br />
+		<label for="pin2">Retype Pin</label>
+		<br />
+		<input
+			type="number"
+			name="pin2"
+			min="0"
+			max="9999"
+			v-model="pin2"
+			required
+		/>
+		<br />
+		<input type="submit" value="Submit" />
+	</form>
+	<p>
+		<router-link to="/login">Login Instead</router-link>
+	</p>
+</template>
+
+<style scoped lang="scss">
+form{
+	
+	input{
+		background-color:rgb(92, 37, 180);
+		border:2px solid #2e2e2e;
+		border-radius:0.5rem;
+		padding-left: 0.5rem;
+		padding-right: 0.5rem;
+		margin-bottom: 0.5rem;
+		color:white;
+	}
+	input[type="submit"]{
+		font-family: 'Montserrat', sans-serif;
+	}
+}
+p{
+	a{
+		color: white;
+		text-decoration: underline;
+	}
+}
+</style>
